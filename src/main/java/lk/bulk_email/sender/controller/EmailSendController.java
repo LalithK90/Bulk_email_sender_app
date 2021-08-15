1da97d5248ae76c7db62867776b784344acdd96b
LalithK90 <asakahatapitiya@gmail.com>
2021-08-15 13:45:55 +0530

    to check cicd

--- src/main/java/lk/bulk_email/sender/controller/EmailSendController.java
+++ src/main/java/lk/bulk_email/sender/controller/EmailSendController.java
@@ -1,16 +1,26 @@
 package lk.bulk_email.sender.controller;
 
 import lk.bulk_email.sender.model.Email;
+import lk.bulk_email.sender.util.service.EmailService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@AllArgsConstructor
 public class EmailSendController {
 
+  private final EmailService emailService;
+
   @PostMapping( "/mailSend" )
-  public String mailSend(@ModelAttribute Email email) {
+  public String mailSend(@ModelAttribute Email email, Model model) {
+    //call to email service and get values
+    model.addAttribute("email_s_count","sample");
+    model.addAttribute("email_r_count","sample");
+    model.addAttribute("message","sample");
     System.out.println(email.toString());
     return "/index";
   }
